c502d508745ec254a9409ba16ea00715c17993d0
Jian Chen <jjccforth@gmail.com>
2016-07-13 20:55:18 -0400

    more fields extracted

--- scraper/process.py
+++ scraper/process.py
@@ -1,11 +1,15 @@
 # -*- coding: UTF-8 -*-
 import re
 
-
-re_p = re.compile(ur'\b(\d+[0-9-_\.]*\d+)\b(?![-_\.])', re.UNICODE)
+re_p = re.compile(ur'([-]*\d+[0-9-_\.]*\d+)\b(?![-_\.])', re.UNICODE)
 
 
 def get_dg(inp):
+    """
+    extract digit part of a string. could be a price or date string
+    :param inp: sting as input
+    :return: digits along with sign
+    """
     output = ""
     matchObj = re_p.search(inp)
     if matchObj:
@@ -17,30 +21,37 @@ def get_dg(inp):
 
 
 def process(l):
-    #print l
+    # print l
     lst = l.split(',')
-    #name
+    # name
     n = lst[0]
-    matchObj = re.match( r'(.*)\((.+)\)', n, re.M|re.I)
+    matchObj = re.match(r'(.*)\((.+)\)', n, re.M | re.I)
     if matchObj:
-        sname =  matchObj.group(1)
-        scode =  matchObj.group(2)
+        sname = matchObj.group(1)
+        scode = matchObj.group(2)
     else:
-       print "No match!!"
+        print "No match!!"
 
-    #price
+    # price
     bp = get_dg(lst[1])
     sp = get_dg(lst[3])
 
-    #date
+    dbp = float(bp)
+    dsp = float(sp)
+    dper = round((dsp-dbp)*100/dbp, 2)
+
+    # date
     dt_b = get_dg(lst[2])
     dt_s = get_dg(lst[4])
 
+    # quantities
+    q = lst[5]
+    # percentage
+    per = get_dg(lst[6])
 
-    print "name %s, code %s ,bp%s, dtb %s, sp %s,dts %s"%(sname,scode,bp,dt_b,sp,dt_s)
-    #print len(lst),n
+    print "name %s, code %s ,bp %s, dtb %s, sp %s,dts %s, q %s, per %s, cal %f" % (sname, scode, bp, dt_b, sp, dt_s, q, per, dper)
+    # print len(lst),n
     pass
-
 
 
 fname = "record.txt"
@@ -50,7 +61,7 @@ count = 0
 for line in iter(f.readline, b''):
     print count,
     process(line)
-    count +=1
+    count += 1
 
-#while (line = f.readline()):
-#    print line
+    # while (line = f.readline()):
+    #    print line
