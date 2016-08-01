0589c1f5099083a12dbf82188443a70c4a5f1acf
Jian Chen <jjccforth@gmail.com>
2016-08-01 15:51:58 -0400

    refactor, extract code into functions

--- scraper/gentouInteractiveTest.py
+++ scraper/gentouInteractiveTest.py
@@ -8,37 +8,22 @@ from selenium.webdriver import ActionChains
 import codecs
 import json
 
-url = "http://www.gentou.com.cn/person/ability/38612"
-
 webdriver.DesiredCapabilities.PHANTOMJS['phantomjs.page.customHeaders.Accept-Language'] = 'en-US,en'
-webdriver.DesiredCapabilities.PHANTOMJS[
-    'phantomjs.page.customHeaders.User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
+webdriver.DesiredCapabilities.PHANTOMJS['phantomjs.page.customHeaders.User-Agent'] \
+    = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
 ghost = webdriver.PhantomJS(executable_path='E:/software/phantomjs-2.0.0-windows/bin/phantomjs.exe')
-ghost.get(url)
 
-try:
-    element = WebDriverWait(ghost, 10).until(EC.presence_of_element_located((By.ID, "lossDiv")))
-finally:
-    print(ghost.find_element_by_id("total").text)
+def get_transaction_from_page(output_file,driver,c_pg,max_pg):
+    """
+    parse a scrapped page and get all the transaction information into an output file
 
-total_txt = ghost.find_element_by_id("total").text
-total_no = int(total_txt.replace('/', ''))
-print "page number total(init)is %d" % total_no
+    :param output_file: output file handler
+    :param driver:  initialized ghost driver
+    :param c_pg:    current page
+    :param max_pg:  maximum page
 
-ghost.save_screenshot("./tmp/mypage.png")
-
-table = ghost.find_elements_by_class_name("tab08")
-
-total_page = ghost.find_element_by_id('total').text
-print total_page
-total_no = int(total_page.replace('/', ''))
-print "page number total(now) is %d" % total_no
-
-# f = open("record.txt","wt+",encoding="UTF-8")
-
-
-
-def get_transaction_from_page(output_file, driver,c_pg,max_pg):
+    :return: 1 normal return, 0 finish during sanity check,
+    """
     # type: (object, object) -> object
     global element
     line = []
@@ -73,12 +58,14 @@ def get_transaction_from_page(output_file, driver,c_pg,max_pg):
         output_file.write(','.join(line))
         output_file.write("###\n")
     # actions = ActionChains(driver).click(next_bt)
+
     page = "./tmp/page%d.png" % c_pg
     driver.save_screenshot(page)
+
     new_page = c_pg + 1
     if new_page > max_pg:
         print "<<<Reach max, return"
-        return
+        return 0
 
 
     next_bt = driver.find_element_by_css_selector('#next')
@@ -91,15 +78,46 @@ def get_transaction_from_page(output_file, driver,c_pg,max_pg):
     driver.save_screenshot(page)
     print "saved %s" % page
     print "<<<Done Waiting"
+    return 1
 
 
-file = codecs.open("record.txt", "w", "utf-8")
 
-for j in range(0, total_no):
-    current_page = ghost.find_element_by_css_selector('.hover.num.on').text
-    print "current page:" + current_page
-    k = 0
-    get_transaction_from_page(file, ghost,int(current_page),total_no)
 
-ghost.close()
-file.close()
+
+def get_agent( url ):
+    global ghost
+    ghost.get(url)
+    try:
+        element = WebDriverWait(ghost, 10).until(EC.presence_of_element_located((By.ID, "lossDiv")))
+    finally:
+        print(ghost.find_element_by_id("total").text)
+    total_txt = ghost.find_element_by_id("total").text
+    total_no = int(total_txt.replace('/', ''))
+    print "page number total(init)is %d" % total_no
+    ghost.save_screenshot("./tmp/mypage.png")
+    table = ghost.find_elements_by_class_name("tab08")
+    total_page = ghost.find_element_by_id('total').text
+    print total_page
+    total_no = int(total_page.replace('/', ''))
+    print "page number total(now) is %d" % total_no
+
+    return total_no
+
+
+if __name__ == '__main__':
+
+    cid = 205691#38612
+    url_address = "http://www.gentou.com.cn/person/ability/%d"%cid
+    total = get_agent(url_address)
+    # f = open("record.txt","wt+",encoding="UTF-8")
+    ofile_name = "record_%d.txt"%cid
+    ofile = codecs.open(ofile_name, "w", "utf-8")
+
+    for j in range(0, total):
+        current_page = ghost.find_element_by_css_selector('.hover.num.on').text
+        print "current page:" + current_page
+        k = 0
+        get_transaction_from_page(ofile, ghost, int(current_page), total)
+
+    ghost.close()
+    ofile.close()
