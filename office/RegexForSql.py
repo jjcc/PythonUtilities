dd6a4b423a9516ab3116aa02b96bd9dcf0348e31
J Chen <jjccforth@gmail.com>
2017-08-10 03:07:33 -0400

    Added group

--- office/RegexForSql.py
+++ office/RegexForSql.py
@@ -1,5 +1,12 @@
 import re
+"""
+For extracting elements of SQL
+"""
 
+KEY_S = "s"
+KEY_F = "f"
+KEY_W = "w"
+KEY_G = "g"
 
 def match_sql(statement):
     '''
@@ -7,15 +14,26 @@ def match_sql(statement):
     '''
     myregex = re.compile(r"SELECT(.+)FROM(.+)WHERE(.+)", re.DOTALL)
     match = myregex.search(statement)
+    groupregex = re.compile(r"GROUP BY(.+)", re.DOTALL)
+    matchgroup = groupregex.search(statement)
+
+    seg = {KEY_S:"", KEY_F:"", KEY_W:"", KEY_G:""}
+
+    if matchgroup:
+        seg[KEY_G] = matchgroup.group(1)
+   
 
     if match:
         print "Match at index %s, %s" % (match.start(), match.end())
-        return (match.group(1), match.group(2), match.group(3))
+        seg[KEY_S] = match.group(1).strip()
+        seg[KEY_F] = match.group(2).strip()
+        seg[KEY_W] = match.group(3).strip()
+
     else:
         print "The regex pattern does not match. :("
-        return (None, None, None)
 
 
+    return seg
 
 
 
@@ -23,8 +41,9 @@ if  __name__ == "__main__":
     sql_statement = '''
 SELECT column1, column2, colume3
 FROM table_name
-WHERE condition; 
+WHERE condition
+GROUP BY group; 
 '''
 
-    (s, f, w) = match_sql(sql_statement)
-    print "Select: %s, From:%s, Where:%s"%(s, f, w)
+    result = match_sql(sql_statement)
+    print "Select: %s, From:%s, Where:%s, Group:%s"%(result[KEY_S], result[KEY_F], result[KEY_W],result[KEY_G])
