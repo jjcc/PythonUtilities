3c72e86385b2f483f50a113c355b2993e13096c0
J Chen <jjccforth@gmail.com>
2017-08-22 00:42:12 -0400

    split fields considering iif which can not use "," to split any more

--- office/RegexForSql.py
+++ office/RegexForSql.py
@@ -37,6 +37,26 @@ def match_sql(statement):
 
 
 
+def split_fields(fields):
+    '''
+    split fields into a list of field
+    '''
+    idx = 0
+    tl = fields
+    while True:
+
+        idx = tl.find(",")
+        if idx < 0:
+            break
+        hdr = tl[0:idx].strip()
+        tl = tl[idx+1:-1]
+        print "h:%s,t:%s"%(hdr,tl)
+        if tl.find("iif") == 0:
+            idx = tl.find(")")
+            tl = tl[idx:-1]
+    pass
+
+
 if  __name__ == "__main__":
     sql_statement = '''
 SELECT column1, column2, colume3
@@ -47,3 +67,8 @@ GROUP BY group;
 
     result = match_sql(sql_statement)
     print "Select: %s, From:%s, Where:%s, Group:%s"%(result[KEY_S], result[KEY_F], result[KEY_W],result[KEY_G])
+
+    flds = '''
+    column1,column2,colunm3,iif(a,b,c)
+    '''
+    split_fields(flds)
