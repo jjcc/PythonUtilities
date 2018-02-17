70a68d16935b51898c47ede1b9c03e97295f56e8
jjcc <jjccforth@gmail.com>
2018-02-16 23:50:12 -0500

    change the misleading name

--- automation/pyscrapimage.py
+++ automation/pyscrapimage.py
@@ -3,38 +3,50 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+import re
 import urllib.request
 
 chrome_path = r"E:\Software\ChromeDriver\chromedriver.exe"
 #target_url =
 stock1 = "http://www.investertech.com/tkchart/tkchart.asp?logo=&home=/default.asp&banner=&stkname=MSFT+INTC+DELL+CSCO+JDSU+ORCL+AMAT+GOOG+IBM+BRCM+AAPL+SYMC"
 
-image1 = "body>table:nth-child(6)>tbody>tr>td:nth-child(1)>table>tbody >tr:nth-child(1)>td:nth-child(1)>img"
-image2 = "body>table:nth-child(6)>tbody>tr>td:nth-child(1)>table>tbody >tr:nth-child(1)>td:nth-child(2)>img"
-image3 = "body>table:nth-child(6)>tbody>tr>td:nth-child(1)>table>tbody >tr:nth-child(1)>td:nth-child(3) > img"
+
 #define driver
 browser = webdriver.Chrome(chrome_path)
 #launch browser
 #browser.get("http://ottawa.craigslist.ca")
 
-browser.get(stock1)
-# wait up to 10 seconds for page to load
-timeout = 10
-try:
-    WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "/html/body/table[4]/tbody/tr/td[1]/table/tbody/tr[3]/td[3]/img")))
-except TimeoutException:
-    print("Timed out waiting for page to load")
-images = browser.find_elements_by_tag_name('img')
-count = 0
-for img in images:
-    imgn = img.get_attribute("src")
-    print("image:" + imgn)
-    count += 1
-    with urllib.request.urlopen(imgn) as url:
-        with open("data/temp%d.gif"%count , 'wb') as f:
-            f.write(url.read())
 
-browser.quit()
+
+
+def get_image_by_url( browser, stock):
+    '''
+    get images from a url in investertech.com
+    :param browser:
+    :param stock:
+    :return:
+    '''
+    browser.get(stock)
+    # wait up to 10 seconds for page to load
+    timeout = 10
+    try:
+        WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "/html/body/table[4]/tbody/tr/td[1]/table/tbody/tr[3]/td[3]/img")))
+    except TimeoutException:
+        print("Timed out waiting for page to load")
+    images = browser.find_elements_by_tag_name('img')
+    count = 0
+    chart_seg = r'chart'
+    for img in images:
+        imgn = img.get_attribute("src")
+        if  not re.search(chart_seg,imgn):
+           continue
+        print("match" + imgn)
+        count += 1
+    #    with urllib.request.urlopen(imgn) as url:
+    #        with open("data/temp%d.gif"%count , 'wb') as f:
+    #            f.write(url.read())
+
+    browser.quit()
 
 
 # get all of the titles for the financial values
@@ -60,4 +72,5 @@ browser.quit()
 # pair each title with its corresponding value using zip function and print each pair
 #for title, value in zip(titles, values):
 #    print(title + ': ' + value)
-
+if __name__ == "__main__":
+    get_image_by_url(browser,stock1)
